a3e62a6c548c57e1ba76f3922856f882e535a9e1
Swapna Gupta <swapna.sony@gmail.com>
2012-07-23 00:52:01 -0500

    First commit

--- jquery.highlight2.js
+++ jquery.highlight2.js
@@ -1,14 +1,14 @@
 /*
+ *
+ *
  * jQuery Highlight plugin
  *
- * Based on highlight v3 by Johann Burkard
- * http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html
+ * Based on jquery.highlight.js by Bartek Szopka
+ * https://github.com/bartaz/sandbox.js
  *
- * Code a little bit refactored and cleaned (in my humble opinion).
- * Most important changes:
- *  - has an option to highlight only entire words (wordsOnly - false by default),
- *  - has an option to be case sensitive (caseSensitive - false by default)
- *  - highlight element tag and class names can be specified in options
+ *
+ * Changes:
+ *  - has an option to specify attributes for the wrapping element.
  *
  * Usage:
  *   // wrap every occurrance of text 'lorem' in content
@@ -36,6 +36,22 @@
  *   // remove custom highlight
  *   $('#content').unhighlight({ element: 'em', className: 'important' });
  *
+ *  //wrap every occurrence of the word 'Lorem' with an anchor tag that has href="#" and title="anchor yeah!"
+ *  $('.content').highlight('Lorem', {element:'a', className:'awesome-tip',
+ *      other_attrs:{href: '#',
+ *      title: 'anchor yeah!'
+ *      }
+ *  });
+ *
+ *
+ *  //wrap every occurrence of the word 'Lorem' with an anchor tag that has href="#" and title="anchor yeah!"
+ *  // and add a class 'foo' to the anchor tag without clobbering the 'awesome-tip' class
+ *  $('.content').highlight('Lorem', {element:'a', className:'awesome-tip',
+ *      other_attrs:{href: '#',
+ *      title: 'anchor yeah!',
+ *      class = function(index, attr){$(this).addClass('awesome-class')}
+ *     }
+ *  });
  *
  * Copyright (c) 2009 Bartek Szopka
  *
@@ -44,12 +60,13 @@
  */
 
 jQuery.extend({
-    highlight: function (node, re, nodeName, className) {
+    highlight: function (node, re, nodeName, className, other_attrs) {
         if (node.nodeType === 3) {
             var match = node.data.match(re);
             if (match) {
                 var highlight = document.createElement(nodeName || 'span');
                 highlight.className = className || 'highlight';
+                $(highlight).attr(other_attrs || {});
                 var wordNode = node.splitText(match.index);
                 wordNode.splitText(match[0].length);
                 var wordClone = wordNode.cloneNode(true);
@@ -58,10 +75,10 @@ jQuery.extend({
                 return 1; //skip added node in parent
             }
         } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
-                !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
-                !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+            !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+            !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
             for (var i = 0; i < node.childNodes.length; i++) {
-                i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
+                i += jQuery.highlight(node.childNodes[i], re, nodeName, className, other_attrs);
             }
         }
         return 0;
@@ -82,15 +99,15 @@ jQuery.fn.unhighlight = function (options) {
 jQuery.fn.highlight = function (words, options) {
     var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
     jQuery.extend(settings, options);
-    
+
     if (words.constructor === String) {
         words = [words];
     }
     words = jQuery.grep(words, function(word, i){
-      return word != '';
+        return word != '';
     });
     words = jQuery.map(words, function(word, i) {
-      return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+        return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
     });
     if (words.length == 0) { return this; };
 
@@ -100,9 +117,9 @@ jQuery.fn.highlight = function (words, options) {
         pattern = "\\b" + pattern + "\\b";
     }
     var re = new RegExp(pattern, flag);
-    
+
     return this.each(function () {
-        jQuery.highlight(this, re, settings.element, settings.className);
+        jQuery.highlight(this, re, settings.element, settings.className, settings.other_attrs);
     });
 };
 
